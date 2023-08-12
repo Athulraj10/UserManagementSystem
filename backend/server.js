e9e86b91ed667f2b80de8310e1d9971a97a3559e
athulraj10 <athulraj165@gmail.com>
2023-08-12 14:19:07 +0530

    git ignore nodemon and .env added

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config()
 
-const PORT =process.env.PORT||5000
+const PORT =process.env.PORT||7000
 
 const app=express()
 app.get('/',(req,res)=>res.send('server is ready'));
