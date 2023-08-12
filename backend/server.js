ec7741fe80e59e9eae6d71fa2a90fa489ccf903e
athulraj10 <athulraj165@gmail.com>
2023-08-12 20:08:37 +0530

    database connected successfully

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,13 @@ import userRoutes from './routes/userRoutes.js'
 import express from 'express';
 import dotenv from 'dotenv';
 import { errorHandler, notFound } from './middleware/errorMiddleware.js';
+import connectDB from './config/db.js';
+
 
 const app=express();
 dotenv.config()
 
+connectDB();
 
 
 app.use('/api/users',userRoutes)
