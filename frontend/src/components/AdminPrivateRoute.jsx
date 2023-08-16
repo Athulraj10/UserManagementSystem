becb9ff6d555025f03fabe05d5000e78ec8c94cb
Athul rajk <107357662+Athulraj10@users.noreply.github.com>
2023-08-16 18:11:27 +0530

    Delete frontend directory

--- frontend/src/components/AdminPrivateRoute.jsx
+++
@@ -1,9 +0,0 @@
-import { Navigate,Outlet } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-
-const AdminPrivateRoute = () =>{
-    const {adminInfo} = useSelector((state)=>state.adminAuth);
-    return adminInfo ? <Outlet/> : <Navigate to='/admin' replace />
-};
-
-export default AdminPrivateRoute;
--- frontend/src/slices/adminSlices/apiSlice.js
+++
@@ -1,9 +0,0 @@
-import { createApi,fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-
-const baseQuery = fetchBaseQuery({baseUrl:''});
-
-export const adminApiSlice = createApi({
-    baseQuery,
-    tagTypes:['Admin'],
-    endpoints:(builder)=>({})
-});
