d6e6e9ad4f791146f5b405069dcb4185847766ae
username <athulraj165@gmail.com>
2023-12-09 19:31:47 +0530

    from group container

--- frontend/src/screens/RegisterScreen.jsx
+++ frontend/src/screens/RegisterScreen.jsx
@@ -66,3 +66,13 @@ const RegisterScreen = () => {
                 onChange={(e)=>{setEmail(e.target.value)}}>
                 </Form.Control>
             </Form.Group>
+
+            <Form.Group className='my-2' controlId='password'>
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                type='password'
+                placeholder='Enter Password'
+                value={password}
+                onChange={(e)=>{setPassword(e.target.value)}}>
+                </Form.Control>
+            </Form.Group>
