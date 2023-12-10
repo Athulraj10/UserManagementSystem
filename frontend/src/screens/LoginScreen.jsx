8bae82872408af4dac2d41be8db98184b6f648cc
username <athulraj165@gmail.com>
2023-12-10 19:08:42 +0530

    importing process completed

--- frontend/src/screens/LoginScreen.jsx
+++ frontend/src/screens/LoginScreen.jsx
@@ -6,3 +6,6 @@ import { useDispatch,useSelector } from 'react-redux';
 import FormContainer from "../components/FormContainer";
 import { useLoginMutation } from '../slices/usersApiSlice';
 import { setCredentials } from '../slices/authSlice';
+import { toast } from 'react-toastify';
+import Loader from '../components/Loader'
+
