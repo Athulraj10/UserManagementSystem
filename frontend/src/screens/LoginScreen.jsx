12e8e306083fbec8c0461f93f2d24bd0ca6e9f36
username <athulraj165@gmail.com>
2023-12-10 19:13:05 +0530

    from container completed

--- frontend/src/screens/LoginScreen.jsx
+++ frontend/src/screens/LoginScreen.jsx
@@ -65,3 +65,13 @@ const LoginScreen = () => {
             <Button type='submit' variant='primary' className='mt-3'>
                 Sign In
             </Button>
+
+            <Row className='py-3'>
+                <Col>
+                    New Customer? <Link to='/register'>Register</Link> 
+                </Col>
+            </Row>            
+        </Form>
+    </FormContainer>
+  )
+}
