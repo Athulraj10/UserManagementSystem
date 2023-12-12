9214e5fec92bc8ecacdb35f8286a7ccca0fabc78
username <athulraj165@gmail.com>
2023-12-12 17:05:16 +0530

    style system in spinner

--- frontend/src/components/Loader.jsx
+++ frontend/src/components/Loader.jsx
@@ -5,5 +5,11 @@ const Loader = () => {
         <Spinner
         animation="border"
         role="status"
+        style={{
+            width:'100px',
+            height:'100px',
+            margin:'auto',
+            display:'block'
+        }}
 
 export default Loader;
