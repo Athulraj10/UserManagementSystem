2bd925e13e4e0206381c8867f7b6c1ebce32d423
username <athulraj165@gmail.com>
2023-12-12 17:06:35 +0530

    user info processing

--- frontend/src/components/PrivateRoute.jsx
+++ frontend/src/components/PrivateRoute.jsx
@@ -2,7 +2,7 @@ import { Navigate,Outlet } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
 const PrivateRoute = () =>{
-
+    const {userInfo} = useSelector((state)=>state.auth);
 };
 
 export default PrivateRoute;
