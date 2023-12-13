4c7dda40ead250287a5694b097015619e4b78d77
username <athulraj165@gmail.com>
2023-12-13 16:56:08 +0530

    hero Returing

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,3 +1,7 @@
 import { Container, Card, Button, Col, Image } from 'react-bootstrap';
 import { useSelector } from 'react-redux';
 import { LinkContainer } from 'react-router-bootstrap';
+
+const Hero = () => {
+
+  const {userInfo} = useSelector((state)=>state.auth)
