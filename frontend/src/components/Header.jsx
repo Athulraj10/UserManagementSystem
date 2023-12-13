e38fb3979d63b0f336d8455a06ddfbde3ecc77d7
username <athulraj165@gmail.com>
2023-12-13 15:59:14 +0530

    header created

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -5,6 +5,28 @@ import { useNavigate } from 'react-router-dom';
 import { LinkContainer } from 'react-router-bootstrap';
 import { useLogoutMutation } from '../slices/usersApiSlice';
 import { logout } from '../slices/authSlice'
+const Header = () => {
+  const {userInfo} = useSelector((state)=>state.auth);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [logoutApiCall] = useLogoutMutation();
+  const logoutHandler = async()=>{
+    try {
+      await logoutApiCall().unwrap();
+      dispatch(logout());
+      navigate('/');
+    } catch (err) {
+      console.log(err)
+    }
+  }
+  return (
+    <header>
+  
+
+  
+    </header>
+  );
+};
 
 
 export default Header;
