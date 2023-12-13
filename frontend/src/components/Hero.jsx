621519d0eb153c7f7fe08e4bcc893c7bfe7062e3
username <athulraj165@gmail.com>
2023-12-13 16:56:42 +0530

    card container on work

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -8,3 +8,5 @@ const Hero = () => {
   
   return (
     <div className=' py-5'>
+      <Container className='d-flex justify-content-center'>
+        <Card className='p-5 d-flex flex-column align-items-center hero-card bg-light w-75'>
