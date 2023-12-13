252d61c8967330238c457931035a4b5912d079e7
username <athulraj165@gmail.com>
2023-12-13 15:55:05 +0530

    return status completed

--- frontend/src/components/AdminPrivateRoute.jsx
+++ frontend/src/components/AdminPrivateRoute.jsx
@@ -3,7 +3,7 @@ import { useSelector } from 'react-redux';
 
 const AdminPrivateRoute = () =>{
     const {adminInfo} = useSelector((state)=>state.adminAuth);
-  
+    return adminInfo ? <Outlet/> : <Navigate to='/admin' replace />
 };
 
 export default AdminPrivateRoute;
