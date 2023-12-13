e1595149f3cd843f64b6bf33fbc669444eb98f38
username <athulraj165@gmail.com>
2023-12-13 15:57:08 +0530

    Row col vise

--- frontend/src/components/FormContainer.jsx
+++ frontend/src/components/FormContainer.jsx
@@ -4,7 +4,10 @@ import { Container,Row,Col } from 'react-bootstrap'
 const FormContainer = ({children}) => {
   return (
     <Container>
-   
+        <Row className='justify-content-md-center mt-5'>
+ 
+ 
+        </Row>
     </Container>
   )
 }
