b0e9ab9624d5997c1fa9f20230e1086d523868b8
username <athulraj165@gmail.com>
2023-12-13 15:54:49 +0530

    return condition pending

--- frontend/src/components/AdminPrivateRoute.jsx
+++ frontend/src/components/AdminPrivateRoute.jsx
@@ -2,6 +2,7 @@ import { Navigate,Outlet } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
 const AdminPrivateRoute = () =>{
+    const {adminInfo} = useSelector((state)=>state.adminAuth);
   
 };
 
