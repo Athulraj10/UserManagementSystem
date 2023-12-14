7874709caff511b535e0cc11271bee8244be5cea
username <athulraj165@gmail.com>
2023-12-14 15:48:39 +0530

    navbar created

--- frontend/src/components/AdminHeader.jsx
+++ frontend/src/components/AdminHeader.jsx
@@ -1,3 +1,8 @@
 import { Navbar, Nav, Container, NavDropdown } from "react-bootstrap";
 import { FaSignInAlt, FaSignOutAlt } from "react-icons/fa";
 import { useSelector, useDispatch } from "react-redux";
+import { LinkContainer } from "react-router-bootstrap";
+import { useAdminLogoutMutation } from "../slices/adminSlices/adminsApiSlice";
+import { adminLogout } from "../slices/adminSlices/adminAuthSlice";
+import { useNavigate } from "react-router-dom";
+
