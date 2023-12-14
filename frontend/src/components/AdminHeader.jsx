a7ee34e658ced396a4170d2fb3203eb5ab92a744
username <athulraj165@gmail.com>
2023-12-14 15:48:17 +0530

    fasigninalt fasignoutalt from react-icons/fa

--- frontend/src/components/AdminHeader.jsx
+++ frontend/src/components/AdminHeader.jsx
@@ -1,1 +1,3 @@
 import { Navbar, Nav, Container, NavDropdown } from "react-bootstrap";
+import { FaSignInAlt, FaSignOutAlt } from "react-icons/fa";
+import { useSelector, useDispatch } from "react-redux";
