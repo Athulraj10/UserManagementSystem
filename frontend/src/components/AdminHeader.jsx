49734d32b6dd8fee8a611ce018da1f5d13bfcaf4
username <athulraj165@gmail.com>
2023-12-14 15:49:44 +0530

    return funtion

--- frontend/src/components/AdminHeader.jsx
+++ frontend/src/components/AdminHeader.jsx
@@ -9,6 +9,22 @@ import { useNavigate } from "react-router-dom";
 function AdminHeader() {
   const { adminInfo } = useSelector((state) => state.adminAuth);
 
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const [logoutApi] = useAdminLogoutMutation();
+
+  const logoutHandler = async () => {
+    try {
+      await logoutApi().unwrap();
+      dispatch(adminLogout());
+      navigate("/admin/login");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  );
 }
 
 export default AdminHeader;
